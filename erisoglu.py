43e6340f3b4526e51d276a8707b7c55d664b2348
Simon H <github@pointbeing.net>
2019-03-20 22:00:05 +0000

    Add initial file for Erisoglu

---
+++ erisoglu.py
@@ -0,0 +1,38 @@
+import numpy as np
+import utils as pu
+
+#
+# Erisoglu 2001: A new algorithm for initial cluster centers in k-means algorithm
+# See: https://www.sciencedirect.com/science/article/pii/S0167865511002248
+#
+
+def variation_coefficient(vector):
+    '''Description of function and note about please, no standardised data'''
+
+    # stddev
+    S = np.std(vector)
+    print("stddev is:", S, "\n")
+
+    # mean = ...
+
+    # foo = stddev / mean
+
+    # bar = abs(foo)
+
+    # return bar
+
+
+
+
+# ------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+
+    data = [1, 2, 3]
+
+    foo = variation_coefficient(data)
+    print("CVj is:", foo)
+
+    #data = pu.get_learning_data()
+
