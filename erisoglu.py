6790eac903264a973eaf064482ede08dea9ac7e9
Simon H <github@pointbeing.net>
2019-03-27 15:34:11 +0000

    Add correlation coefficient

--- erisoglu.py
+++ erisoglu.py
@@ -1,51 +1,71 @@
 import numpy as np
-import utils as pu
-import scipy as sp
+#import scipy as sp
 
-#
 # Erisoglu 2011 "new" algorithm:
 # See: A new algorithm for initial cluster centers in k-means algorithm
 # https://www.sciencedirect.com/science/article/pii/S0167865511002248
-#
 
 class Erisoglu():
 
-    def _variation_coefficient(self, vector):
-        '''Absolute value of std dev / mean. Data must not be standardised'''
-     
-        return abs(sp.stats.variation(vector))
-   
-   
-    def main_axis(self, data):
-        '''Step i) Find the column/feature with greatest variance'''
-        
-        data = self._prepare_data(data)
-        
+    # Main steps of the algorithm
+    # nb. these assume the data to be an ndarray and transposed
+
+    def find_main_axis(self, data):
+        '''Step i) Find the feature with greatest variance'''
+
         max = 0
-        column = None
-        
+        max_column = None
+
         for j in range(0, len(data)):
-            cvj = self._variation_coefficient(data[j])
-            
+
+            cvj = self.variation_coefficient(data[j])
+
             if cvj > max:
                 max = cvj
-                column = j
-                
-        return j
-        
-        
-    #def secondary_axis(self, data):
-    
-    #    main = self._main_axis(data)
-        
-    #    data = self._prepare_data(data)
-        
-    #    for each column etc...
-        
-        
+                max_column = j
+
+        return max_column
+
+
+    # Supporting calculations etc ----------------------------------------------
+
+    def variation_coefficient(self, vector):
+        '''Absolute value of std dev / mean.'''
+
+        return abs(np.std(vector) / np.mean(vector))
+
+
+    def correlation_coefficient(self, left, right): # column j, column j'
+        '''Correlation coefficient between two vectors'''
+
+        numerator = 0
+        denominator_left = 0
+        denominator_right = 0
+
+        for i in range(0, len(left)):
+
+            dev_left = (left[i] - np.mean(left))
+            dev_right = (right[i] - np.mean(right))
+
+            numerator +=  dev_left * dev_right
+
+            denominator_left += dev_left ** 2
+            denominator_right += dev_right ** 2
+
+        # TODO: This is where Erisoglu seems to differ from Pearson
+        denominator = denominator_left**0.5 * denominator_right**0.5
+        #denominator = denominator_left * denominator_right
+
+        return (numerator / denominator)
+
+
     def _prepare_data(self, data):
         '''Since we're working with columns, it's simpler if we transpose first'''
-    
-        return np.array(data).T      
-               
 
+        return np.array(data).T
+
+# ------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    pass
--- tests/test_erisoglu.py
+++ tests/test_erisoglu.py
@@ -6,37 +6,46 @@ class ErisogluTestSuite(unittest.TestCase):
     def setUp(self):
         self._e = Erisoglu()
         self._set_up_data()
-        
 
-    def test_find_main_axis(self):  
-        self.assertEqual(self._e.main_axis(self._data1), 4)
-        self.assertEqual(self._e.main_axis(self._data2), 5)
-        
-    # TODO   
+    def test_variation_coefficient(self):
+        self.assertEqual(self._e.variation_coefficient([1,1,1]), 0)
+        self.assertAlmostEqual(self._e.variation_coefficient([1,2,3]), 0.40824829046)
+        self.assertAlmostEqual(self._e.variation_coefficient([-1,-2,-3]), 0.40824829046)
+
+    def test_find_main_axis(self):
+        self.assertEqual(self._e.find_main_axis(self._data1), 3)
+        self.assertEqual(self._e.find_main_axis(self._data2), 4)
+
+    # Note discrepancy between Erisoglu and Pearson. Currently I've used Pearson
+    def test_correlation_coefficient(self):
+            self.assertAlmostEqual(self._e.correlation_coefficient([1,2], [2,4]), 1)
+            self.assertAlmostEqual(self._e.correlation_coefficient([2,1], [2,4]), -1)
+            self.assertAlmostEqual(self._e.correlation_coefficient([1,2,3,4,5], [2,4,6,8,10]), 1)
+            self.assertAlmostEqual(self._e.correlation_coefficient([10,2,3,4,5,6,99], [1,2,3,4,3,2,1]), -0.546, 4)
+
     #def test_find_secondary_axis(self):
-        #self.assertEqual(self._e.secondary_axis(self._data1), 4)
-        #self.assertEqual(self._e.secondary_axis(self._data2), 5)
-  
-    # misc setup methods -------------------------------------------------------
-            
-    def _set_up_data(self):
-    
-        self._data1 = [
-            [1, 2, 3, 1000, 9],
-            [1, 3, 2,  -44, 8],
-            [1, 2, 3, 1001, 7],
-            [1, 3, 1, -100, 8]
-        ]
+     #   self.assertEqual(self._e.find_secondary_axis(self._data1), 4)
+        #self.assertEqual(self._e.find_secondary_axis(self._data2), 5)
 
-        self._data2 = [
+    # misc setup methods -------------------------------------------------------
+
+    def _set_up_data(self):
+
+        self._data1 = self._e._prepare_data([
+            [1, 2, 3, 1000, 9],
+            [1, 3, 2, -999, 8],
+            [1, 2, 3, 1001, 7],
+            [1, 3, 1, -998, 8]
+        ])
+
+        self._data2 = self._e._prepare_data([
             [9, 1, 2, 3, 1000, 9],
-            [9, 1, 3, 2,  -44, 8],
+            [9, 1, 3, 2, -999, 8],
             [7, 1, 2, 3, 1001, 7],
-            [8, 1, 3, 1, -100, 8]
-        ]
-                
+            [8, 1, 3, 1, -998, 8]
+        ])
+
 # ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     unittest.main()
-    
