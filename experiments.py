f3383523c198c4202a1d20fb26721283f1b79ae7
Simon H <github@pointbeing.net>
2019-04-06 22:13:00 +0100

    Initial experiments with ARI etc

---
+++ experiments.py
@@ -0,0 +1,38 @@
+from sklearn import datasets
+import sklearn.cluster as cluster
+import sklearn.metrics as metrics
+import kmeans
+import utils
+
+'''
+See:
+https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+'''
+
+dataset = datasets.load_iris()
+#wine = datasets.load_wine()
+#bc = datasets.load_breast_cancer()
+
+K =3
+
+data = utils.standardise(dataset.data)
+target = dataset.target
+
+Z, U, clusters, iterations = kmeans.cluster(data, K)
+
+est1 = cluster.KMeans(n_clusters=K, n_init=1, init='random')
+est1.fit(data)
+
+est2 = cluster.KMeans(n_clusters=K)
+est2.fit(data)
+
+print('Mine:', U)
+print("SKL (naive): ", est1.labels_)
+print("SKL (smarter): ", est2.labels_)
+print("Target:", target)
+
+score_me = metrics.adjusted_rand_score(target, U)
+score_them_n = metrics.adjusted_rand_score(target, est1.labels_)
+score_them_s = metrics.adjusted_rand_score(target, est2.labels_)
+
+print("Me:", score_me, "| SKLearn (naive):", score_them_n, "| SKLearn (smarter):", score_them_s)
