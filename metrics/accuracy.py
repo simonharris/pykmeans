ac35b8cb69f914dc7d52a782872e59665088e9d7
Simon H <github@pointbeing.net>
2019-04-24 22:39:33 +0100

    Move to vectorised solution

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -3,16 +3,4 @@ import numpy as np
 #TODO: ask Renato the name of the algorithm
 
 def from_matrix(mat):
-
-    total = np.sum(mat)
-    hits = 0
-
-    while len(mat) > 0:
-
-        row, col = np.unravel_index(np.argmax(mat, axis=None), mat.shape)
-        hits += mat[row, col]
-
-        mat = np.delete(mat, row, axis=0)
-        mat = np.delete(mat, col, axis=1)
-
-    return hits/total
+    return np.sum(np.max(mat, 1)) / np.sum(mat)
