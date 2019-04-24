ae35ff967b7462a0b88fc16ee27811bffff009ad
Simon H <github@pointbeing.net>
2019-04-25 00:37:52 +0100

    Add Wine dataset and modify accuracy function a little

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -3,4 +3,4 @@ import numpy as np
 #TODO: ask Renato the name of the algorithm
 
 def from_matrix(mat):
-    return np.sum(np.max(mat, 1)) / np.sum(mat)
+    return np.sum(np.max(mat, 0)) / np.sum(mat)
--- tests/metrics/test_accuracy.py
+++ tests/metrics/test_accuracy.py
@@ -6,9 +6,17 @@ class AccuracyScoreTest(unittest.TestCase):
 
     def test_accuracy_score(self):
 
+        # Yuan 2004 Iris
         cm = np.array(
                 [[ 0,  0, 50],
                  [ 3, 47,  0],
                  [36, 14,  0]])
 
-        self.assertEqual(accuracy.from_matrix(cm), 0.8866666666666667)
+        self.assertAlmostEqual(accuracy.from_matrix(cm), 0.886667, places=6)
+
+        # Yuan 2004 Wine
+        cm = np.array(
+                [[31,  1, 27],
+                 [ 7, 64,  0],
+                 [11, 37,  0]])
+        self.assertAlmostEqual(accuracy.from_matrix(cm), 0.685393, places=6)
