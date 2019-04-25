060da2a17769ff23236bc1b96356354303f0577a
Simon H <github@pointbeing.net>
2019-04-25 10:31:06 +0100

    Remove deprecated function

--- utils.py
+++ utils.py
@@ -1,24 +1,11 @@
 import numpy as np
 from sklearn import preprocessing
 
-#
-# General purpose utils for pycluster
-#
-
-# Data loading functions -------------------------------------------------------
-
-
-def get_learning_data():
-    '''Handy learning data from Renato's CE705 Python assignment'''
-
-    # Note: this will not currently work outside of unit test as it is
-    # not a SKLearn Bunch, e.g. no 'target' attribute'
-    
-    return np.loadtxt('sample_data/Learning_Data.csv', delimiter=',', dtype='float')
-
+'''
+General purpose utils for pycluster
+'''
 
 # Data preprocessing functions -------------------------------------------------
-
 
 def standardise(data):
     '''Scale data from -1 to 1, with 0 mean and unit variance'''
