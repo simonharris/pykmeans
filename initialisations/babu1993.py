57691ebd2147dd6644c72ebec5c7f0069b724c0b
Simon H <github@pointbeing.net>
2019-05-16 16:46:40 +0100

    Add initial Babu skeleton files

---
+++ initialisations/babu1993.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+'''
+Babu 1993 genetic algorithm
+
+See: A near-optimal initial seed value selection in K-means means algorithm
+using a genetic algorithm
+https://www.sciencedirect.com/science/article/abs/pii/016786559390058L
+'''
+
+#
