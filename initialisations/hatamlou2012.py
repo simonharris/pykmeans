6f61f26c3d9e1129fedb9a1b2c7a4fe18837ef33
Simon H <github@pointbeing.net>
2019-05-16 16:45:09 +0100

    Typo fix

--- initialisations/hatamlou2012.py
+++ initialisations/hatamlou2012.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 '''
-Hatamlou 2012 binary search algorithm algorithm
+Hatamlou 2012 binary search algorithm
 
 See: In search of optimal centroids on data clustering using a binary search algorithm
 https://www.sciencedirect.com/science/article/abs/pii/S0167865512001961
