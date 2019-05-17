3dfe53612a3a317376e23a0192d54691ee51cca4
Simon H <github@pointbeing.net>
2019-05-17 19:43:20 +0100

    Initial add of Khan notebook

---
+++ initialisations/kahn2012.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+'''
+Kahn 2012 initial seed selection algorithm
+
+See: An initial seed selection algorithm for k-means clustering of georeferenced data to improve replicability of cluster assignments for mapping application
+https://www.sciencedirect.com/science/article/pii/S1568494612003377
+'''
+
+#
