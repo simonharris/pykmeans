7721ed2c7979c34988a22b8e1e137ac097589c18
Simon H <github@pointbeing.net>
2019-06-01 16:51:04 +0100

    Add first three steps for Khan 2012

--- initialisations/khan2012.py
+++ initialisations/khan2012.py
@@ -1,10 +1,34 @@
 import numpy as np
+from scipy.spatial import distance as spdistance
 
 '''
-Kahn 2012 initial seed selection algorithm
+Khan 2012 initial seed selection algorithm
 
 See: An initial seed selection algorithm for k-means clustering of georeferenced data to improve replicability of cluster assignments for mapping application
 https://www.sciencedirect.com/science/article/pii/S1568494612003377
 '''
 
-#
+def sort_by_magnitude(data):
+    ''' Step i) Sort data by magnitude'''
+    idxs = np.argsort(np.linalg.norm(data, axis=1))
+
+    return data[idxs]
+
+
+def find_distances(data):
+    '''Step ii) Find Euclidean distances between sorted rows'''
+
+    distances = []
+
+    for i in range(0, len(data)-1):
+        distances.append(spdistance.euclidean(data[i], data[i+1]))
+
+    return np.array(distances)
+
+
+def find_furthest(distances, K):
+    '''Step iii) Find the largest distances between adjacent rows'''
+
+    howmany = K-1
+
+    return distances.argsort()[-howmany:][::-1]
---
+++ tests/initialisations/test_khan.py
@@ -0,0 +1,43 @@
+import unittest
+from initialisations import khan2012 as khan
+import numpy as np
+import sklearn.datasets as skdatasets
+
+class BabuTestSuite(unittest.TestCase):
+
+    def setUp(self):
+        self._setUpData()
+
+
+    def test_sort_by_magnitude(self):
+        np.testing.assert_equal(khan.sort_by_magnitude(self._data), self._sorted)
+
+
+    def test_find_distances(self):
+        distances = khan.find_distances(self._sorted)
+        expected = np.array([1.7320508075688772, 3.1622776601683795, 11.74734012447073])
+        np.testing.assert_equal(distances, expected)
+
+    def test_find_furthest(self):
+        distances = khan.find_distances(self._sorted)
+
+        np.testing.assert_equal(khan.find_furthest(distances, 3), [2, 1])
+
+
+    # Utilities ----------------------------------------------------------------
+
+    def _setUpData(self):
+        self._data = np.array([
+                [10,10,10],
+                [ 1, 1, 1],
+                [ 2, 2, 2],
+                [ 5, 3, 2],
+            ])
+
+        self._sorted = np.array([
+                [ 1, 1, 1],
+                [ 2, 2, 2],
+                [ 5, 3, 2],
+                [10,10,10],
+            ])
+
