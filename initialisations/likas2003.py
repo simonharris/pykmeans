6de3d184c937f7ba54a43c48a98a097139ea48f4
Simon H <github@pointbeing.net>
2019-06-02 18:47:34 +0100

    Initial start on Likas 2003

---
+++ initialisations/likas2003.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+'''
+Likas 1993 Global K-Means algorithm
+
+The global k-means clustering algorithm
+https://www.sciencedirect.com/science/article/pii/S0031320302000602
+'''
+
+# See Jupyter Notebook 08_Likas
