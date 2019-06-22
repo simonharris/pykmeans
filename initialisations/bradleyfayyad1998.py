d7b3cce215d6d50bbca28961d702afacbd4c9baf
Simon H <github@pointbeing.net>
2019-06-22 22:07:44 +0100

    Initial files for Bradley & Fayyad 1998

---
+++ initialisations/bradleyfayyad1998.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+'''
+Bradley & Fayyad 1998
+
+See: Refining Initial Points for K-Means Clustering
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.50.8528&rep=rep1&type=pdf
+'''
+
+#
+
+
