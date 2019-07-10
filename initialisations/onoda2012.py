cc84e6ec92c957c5927b8ac1ba9cf7371f7ca331
Simon H <github@pointbeing.net>
2019-07-10 15:26:44 +0100

    Initial Onoda files

---
+++ initialisations/onoda2012.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+'''
+Onoda 2012 ICA-based algorithm
+
+See: Careful seeding method based on independent components analysis for k-means clustering
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.663.5343&rep=rep1&type=pdf#page=53
+'''
+
+#
