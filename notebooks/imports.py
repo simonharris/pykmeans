f6dc8b82662aa9bd2acf824839492d64d0ca80cf
Simon H <github@pointbeing.net>
2019-07-31 17:00:45 +0100

    Tidy up a little

--- notebooks/imports.py
+++ notebooks/imports.py
@@ -1,6 +1,14 @@
-# Yuck, but see: https://stackoverflow.com/questions/34478398
+'''
+Allow imports from parent directory
+
+Yuck, but see: https://stackoverflow.com/questions/34478398
+'''
+
 import os
 import sys
+
 module_path = os.path.abspath(os.path.join('..'))
+
 if module_path not in sys.path:
     sys.path.append(module_path)
+
