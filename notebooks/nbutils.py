2f18ccbef6e45495d34ecc255b18588f3c44377c
Simon H <github@pointbeing.net>
2019-08-06 23:24:57 +0100

    Integrate soybean-small

--- notebooks/nbutils.py
+++ notebooks/nbutils.py
@@ -6,7 +6,7 @@ from datasets import loader
 from metrics import accuracy, nmi, ari
 
 
-SEP = '=====================================================\n'
+SEP = '====================================================='
 
 
 def run_clustering(algorithm, opts={}):
@@ -14,6 +14,7 @@ def run_clustering(algorithm, opts={}):
 
     _run_dataset('HART', loader.load_hartigan(), 3, algorithm, opts)
     _run_dataset('IRIS', loader.load_iris(), 3, algorithm, opts)
+    _run_dataset('SOYS', loader.load_soy_small(), 4, algorithm, opts)
     _run_dataset('WINE', loader.load_wine(), 3, algorithm, opts)
     _run_dataset('WBCO', loader.load_wbco(), 2, algorithm, opts)
 
@@ -29,14 +30,14 @@ def _run_dataset(name, dataset, K, algorithm, opts):
 
     centroids = algorithm.generate(data, K, opts)
 
-    print("Initial seeds:")
-    print(centroids, "\n")
+    #print("Initial seeds:")
+    #print(centroids, "\n")
 
     est = KMeans(n_clusters=K, n_init=1, init=centroids)
     est.fit(data)
     
-    print("Discovered centroids:")
-    print(est.cluster_centers_, "\n")
+    #print("Discovered centroids:")
+    #print(est.cluster_centers_, "\n")
     
     _run_metrics(target, est)
 
@@ -51,6 +52,5 @@ def _run_metrics(target, est):
     print("Accuracy Score:", acc)
     print("Adjusted Rand Index:", asc)
     print("NMI:", nsc)
-    print("Inertia:", est.inertia_)
-    print("\n")
-
+    print("Inertia:", est.inertia_, "\n")
+    
