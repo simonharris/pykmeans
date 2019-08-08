1183edfa37b8509cd3d4cbf29e94e02120056b23
Simon H <github@pointbeing.net>
2019-08-08 14:25:21 +0100

    Add initial Hand files

---
+++ initialisations/hand2005.py
@@ -0,0 +1,10 @@
+'''
+Hand 2006 ICA-based algorithm
+
+See: Optimising k-means clustering results with standard software packages
+https://www.sciencedirect.com/science/article/pii/S0167947304002038
+'''
+
+import numpy as np
+
+
