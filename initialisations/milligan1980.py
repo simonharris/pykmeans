00a93f0507453966edd90507590866a6528d6b74
Simon H <github@pointbeing.net>
2019-08-08 12:09:19 +0100

    Move Milligan code to initialisation file and create standard notebook

--- initialisations/milligan1980.py
+++ initialisations/milligan1980.py
@@ -5,4 +5,19 @@ See: The validation of four ultrametric clustering algorithms
 https://www.sciencedirect.com/science/article/abs/pii/0031320380900011
 '''
 
-#
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+
+
+def generate(data, K, opts={}):
+
+    ward = AgglomerativeClustering(n_clusters=K, linkage='ward')
+    ward.fit(data)
+    
+    centroids = np.zeros((K, data.shape[1]))
+
+    for k in range(0, K):
+        centroids[k,:] = np.mean(data[ward.labels_==k], axis=0)
+        
+    return centroids
+    
