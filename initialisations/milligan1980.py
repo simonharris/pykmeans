fe04f87a74dfb46d8fa715844e5be765830af3c3
Simon H <github@pointbeing.net>
2019-08-08 11:41:06 +0100

    Initial files for Milligan 1980

---
+++ initialisations/milligan1980.py
@@ -0,0 +1,8 @@
+'''
+Milligan 1980 Ward-based algorithm
+
+See: The validation of four ultrametric clustering algorithms
+https://www.sciencedirect.com/science/article/abs/pii/0031320380900011
+'''
+
+#
