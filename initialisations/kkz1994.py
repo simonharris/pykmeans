35b25cf85877d3af9679ee8746c51d9d7b9b47d7
Simon H <github@pointbeing.net>
2019-08-16 21:21:31 +0100

    Move KKZ code to proper file

--- initialisations/kkz1994.py
+++ initialisations/kkz1994.py
@@ -7,5 +7,22 @@ https://ieeexplore.ieee.org/abstract/document/329844/
 
 import numpy as np
 
-#
+from kmeans import distance_table
 
+
+def generate(data, K, opts={}):
+
+    norms = np.linalg.norm(data, axis=1)
+
+    first = data[np.argmax(norms)]
+    codebook = np.array([first])
+
+    while codebook.shape[0] < K:
+        distances = distance_table(data, codebook)
+        mins = np.min(distances, axis=1)
+        amax = np.argmax(mins, axis=0)
+        nxt = data[amax]
+        codebook = np.append(codebook, [nxt], axis=0)
+        
+    return codebook
+    
