d33cc4339b3ba329da5286d8a576a063f6b0e386
Simon H <github@pointbeing.net>
2019-08-16 18:22:35 +0100

    Initial files for KKZ

---
+++ initialisations/kkz1994.py
@@ -0,0 +1,11 @@
+'''
+KKZ 1994 algorithm
+
+See: A new initialization technique for generalized Lloyd iteration
+https://ieeexplore.ieee.org/abstract/document/329844/
+'''
+
+import numpy as np
+
+#
+
