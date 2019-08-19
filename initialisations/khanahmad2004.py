d56214cad082359626f3b5b9c0858a88b0ac7d8e
Simon H <github@pointbeing.net>
2019-08-19 19:37:15 +0100

    Initial add of Khan & Ahmad 2004 files

---
+++ initialisations/khanahmad2004.py
@@ -0,0 +1,16 @@
+'''
+Khan & Ahmad 2004 CCIA algorithm
+
+See: Cluster center initialization algorithm for K-means clustering
+https://www.sciencedirect.com/science/article/abs/pii/S0167865504000996
+'''
+
+import numpy as np
+
+#
+
+
+def generate(data, K, opts={}):
+    pass
+    
+    
