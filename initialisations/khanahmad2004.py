d24df68f6671075209a7e95b0eb75a775a9a46c3
Simon H <github@pointbeing.net>
2019-08-20 13:39:47 +0100

    A bit more work on CCIA

--- initialisations/khanahmad2004.py
+++ initialisations/khanahmad2004.py
@@ -1,8 +1,11 @@
 '''
-Khan & Ahmad 2004 CCIA algorithm
+Khan & Ahmad 2004 "Cluster center initialization algorithm"
 
 See: Cluster center initialization algorithm for K-means clustering
 https://www.sciencedirect.com/science/article/abs/pii/S0167865504000996
+
+Shamelessly copied from:
+https://github.com/titubeta/ccia/
 '''
 
 import numpy as np
