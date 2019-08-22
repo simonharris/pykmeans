13dd8cddbc1d13d1839fa127d026bc6dc771f0fd
Simon H <github@pointbeing.net>
2019-08-22 14:50:56 +0100

    Move Hatamlou to proper initialisation file

--- tests/initialisations/test_hatamlou.py
+++ tests/initialisations/test_hatamlou.py
@@ -1,18 +1,24 @@
+"""Plenty to be done here"""
+
 import unittest
+
 import numpy as np
-from initialisations import hatamlou2012 as hatamlou
+
+from initialisations.hatamlou2012 import Hatamlou
 
 
 class HatamlouTestCase(unittest.TestCase):
 
     def test_min_max(self):
         '''Sanity check that numpy does what I expected'''
-        
+ 
         data = np.array([[1.0,2.0,3.0],
                          [0.5,3.0,4.0],
                          [2.0,4.0,0.1]])
-
-        min, max = hatamlou.find_min_max(data)
+       
+        htmlu = Hatamlou(data, 3, {'max_loops': 1})      
+ 
+        min, max = htmlu._find_min_max(data)
         self.assertListEqual(list(min), [0.5,2.0,0.1])
         self.assertListEqual(list(max), [2.0,4.0,4.0])
 
