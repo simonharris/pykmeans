bc34cef8ea5a85795019e676ee8bbb1caacaf4c1
Simon H <github@pointbeing.net>
2019-08-31 17:57:23 +0100

    Add the CCIA Iris data, as it differs from sklearn

--- datasets/loader.py
+++ datasets/loader.py
@@ -2,7 +2,9 @@
 
 import os
 
+import arff
 import numpy as np
+import pandas as pd
 import sklearn.datasets as skdatasets
 
 
@@ -11,42 +13,52 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class Dataset():
     '''Mimics the sklearn dataset interface'''
-    
+
     def __init__(self, data, target):
         self.data = data
         self.target = target
-        
+
 
 def _load_local(which):
 
     datafile = dir_path + '/' + which + '/data.csv'
     labelfile = dir_path + '/' + which + '/labels.csv'
-    
+
     return Dataset(
         np.loadtxt(datafile, delimiter=',', dtype=np.int),
         np.loadtxt(labelfile, delimiter=',', dtype=np.int),
     )
 
 
-## The loaders to be exposed --------------------------------------------------- 
-    
+# The loaders to be exposed ---------------------------------------------------
+
 
 def load_iris():
     return skdatasets.load_iris()
-    
-def load_iriska():
-    return _load_local('iriska')
+
+
+def load_iris_ccia():
+    """To ensure using the exact same Iris data as Khan & Ahmad 2004"""
+
+    datafile = dir_path + '/iris_ccia/iris.arff'
+
+    with open(datafile) as df:
+        iris = np.array(arff.load(df)['data'])
+
+    return Dataset(iris[:, 0:4].astype('float'), pd.factorize(iris[:, 4]))
+
 
 def load_wine():
     return skdatasets.load_wine()
 
+
 def load_hartigan():
     return _load_local('hartigan1975')
-    
+
+
 def load_soy_small():
     return _load_local('soy_small')
 
-def load_wbco():
-    return _load_local('wbco') 
-    
 
+def load_wbco():
+    return _load_local('wbco')
