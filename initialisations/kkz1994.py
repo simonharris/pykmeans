192008600ec6c2a277fe0ae261a979820a63de8d
Simon H <github@pointbeing.net>
2019-09-05 17:58:33 +0100

    Bring KKZ up to coding standards

--- initialisations/kkz1994.py
+++ initialisations/kkz1994.py
@@ -1,28 +1,42 @@
-'''
+"""
 KKZ 1994 algorithm
 
 See: A new initialization technique for generalized Lloyd iteration
 https://ieeexplore.ieee.org/abstract/document/329844/
-'''
+"""
 
 import numpy as np
 
 from kmeans import distance_table
+from initialisations.base import Initialisation
 
 
-def generate(data, K, opts={}):
+class KKZ(Initialisation):
+    """KKZ 1994 initialisation algorithm"""
 
-    norms = np.linalg.norm(data, axis=1)
+    def find_centers(self):
+        """Main method"""
 
-    first = data[np.argmax(norms)]
-    codebook = np.array([first])
+        norms = np.linalg.norm(self._data, axis=1)
 
-    while codebook.shape[0] < K:
-        distances = distance_table(data, codebook)
-        mins = np.min(distances, axis=1)
-        amax = np.argmax(mins, axis=0)
-        nxt = data[amax]
-        codebook = np.append(codebook, [nxt], axis=0)
-        
-    return codebook
-    
+        first = self._data[np.argmax(norms)]
+        codebook = np.array([first])
+
+        while codebook.shape[0] < self._num_clusters:
+            distances = distance_table(self._data, codebook)
+            mins = np.min(distances, axis=1)
+            amax = np.argmax(mins, axis=0)
+            nxt = self._data[amax]
+            codebook = np.append(codebook, [nxt], axis=0)
+
+        return codebook
+
+
+# -----------------------------------------------------------------------------
+
+
+def generate(data, num_clusters, opts):
+    """The common interface"""
+
+    kkz = KKZ(data, num_clusters, opts)
+    return kkz.find_centers()
