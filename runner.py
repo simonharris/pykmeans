46a100a09927d1a54a3db3f9fcb8fa9bc2ad86a7
Simon H <github@pointbeing.net>
2019-09-05 16:35:31 +0100

    Minimal changes; just cleaning up

--- runner.py
+++ runner.py
@@ -1,18 +1,10 @@
-'''Quick CLI utility to run an initialisation against a dataset'''
+"""Quick CLI utility to run an initialisation against a dataset"""
 
-from sklearn import preprocessing
-    
 from datasets import loader
-#from initialisations import macqueen1967 as algorithm
 from initialisations import khanahmad2004 as algorithm
 
-
-min_max_scaler = preprocessing.MinMaxScaler()
-
-
-dataset = loader.load_iris()
+dataset = loader.load_iris_ccia()
 data = dataset.data
-#data = min_max_scaler.fit_transform(data)
 
 K = 3
 
@@ -20,4 +12,3 @@ centroids = algorithm.generate(data, K, {})
 
 print("OUTPUT:")
 print(centroids)
-
