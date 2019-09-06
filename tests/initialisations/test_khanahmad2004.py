eb50803174555370f0ebe5e2531ea0de86801628
Simon H <github@pointbeing.net>
2019-09-06 17:14:34 +0100

    Add initial, crude tests for CCIA

--- tests/initialisations/test_khanahmad2004.py
+++ tests/initialisations/test_khanahmad2004.py
@@ -1,16 +1,22 @@
 import unittest
 
-# import numpy as np
-# import sklearn.datasets as skdatasets
+import numpy as np
+import sklearn.datasets as skdatasets
 
 from initialisations import khanahmad2004 as ccia
 
+
 class KhanAhmadTestSuite(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_whole_thing(self):
+        """Crude integration-style test until I can break it down a little"""
 
+        iris = skdatasets.load_iris()
 
+        seeds = ccia.generate(iris.data, 3, {})
 
+        expected = np.array([[5.006, 3.428, 1.462, 0.246],
+                             [6.85384615, 3.07692308, 5.71538462, 2.05384615],
+                             [5.88360656, 2.74098361, 4.38852459, 1.43442623]])
 
-
+        np.testing.assert_array_almost_equal(seeds, expected)
