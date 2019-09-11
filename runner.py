9fc3aaa95ee24bef82737670f7d1390e8cdbf628
Simon H <github@pointbeing.net>
2019-09-11 17:31:38 +0100

    Switch CCIA to Fossil dataset, as per paper

--- runner.py
+++ runner.py
@@ -3,7 +3,8 @@
 from datasets import loader
 from initialisations import khanahmad2004 as algorithm
 
-dataset = loader.load_iris_ccia()
+# dataset = loader.load_iris_ccia()
+dataset = loader.load_fossil()
 data = dataset.data
 
 K = 3
