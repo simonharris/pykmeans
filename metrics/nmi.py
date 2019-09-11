a5b50ee8ec7f303ae578bed028bee7838920b78e
Simon H <github@pointbeing.net>
2019-09-11 17:31:07 +0100

    Fixes for pylint

--- metrics/nmi.py
+++ metrics/nmi.py
@@ -1,8 +1,9 @@
-'''Normalized mutual information score'''
+"""Normalized mutual information score"""
 
 from sklearn.metrics.cluster import normalized_mutual_info_score as nmi
 
 
 def score(target, found):
-    return nmi(target, found, average_method='arithmetic')
+    """The common interface"""
 
+    return nmi(target, found, average_method='arithmetic')
