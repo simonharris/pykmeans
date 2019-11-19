48df55a0d2d840f212881ee1028cc0d39953a6f4
Simon H <github@pointbeing.net>
2019-11-19 11:02:25 +0000

    Improve for PEP6/lint

--- initialisations/onoda2012.py
+++ initialisations/onoda2012.py
@@ -1,62 +1,61 @@
-'''
+"""
 Onoda 2012 ICA-based algorithm
 
-See: Careful seeding method based on independent components analysis for k-means clustering
+See: Careful seeding method based on independent components analysis for
+ k-means clustering
 http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.663.5343&rep=rep1&type=pdf#page=53
-'''
+"""
 
 import numpy as np
 from sklearn.decomposition import FastICA, PCA
 
 
-def _run_pca(data, K):
-    '''Run Pricipal Component Analysis'''
+def _run_pca(data, num_components):
+    """Run Pricipal Component Analysis"""
 
-    pca = PCA(n_components=K)
+    pca = PCA(n_components=num_components)
     pca.fit(data)
     return pca.components_
 
 
-def _run_ica(data, K):
-    '''Run Independent Component Analysis'''
-    
-    ica = FastICA(n_components=K) 
-    ica.fit_transform(data) 
+def _run_ica(data, num_components):
+    """Run Independent Component Analysis"""
+
+    ica = FastICA(n_components=num_components)
+    ica.fit_transform(data)
     return ica.components_
 
 
 def _find_centroids(data, components):
-    '''Step 1b from the algorithm'''
+    """Step 1b from the algorithm"""
 
-    C = []
+    centroids = []
 
     for component in components:
         distances = [_calc_distance(x, component) for x in data]
-        C.append(data[np.argmin(distances)])
+        centroids.append(data[np.argmin(distances)])
 
-    return np.array(C)
+    return np.array(centroids)
 
 
 def _calc_distance(row, component):
-    '''Used in Step 1b from the algorithm'''
-    
+    """Used in Step 1b from the algorithm"""
+
     mag = np.linalg.norm
-    
+
     return np.dot(component, row) / (mag(component) * mag(row))
 
 
-# Main interface ---------------------------------------------------------------
+# Main interface --------------------------------------------------------------
 
-    
-def generate(data, K, opts={'method':'ICA'}):
-    '''Provide consistent interface'''
 
-    if (opts['method'] == 'PCA'):
+def generate(data, K, opts={'method': 'ICA'}):
+    """Provide consistent interface"""
+
+    if opts['method'] == 'PCA':
         components = _run_pca(data, K)
     else:
         components = _run_ica(data, K)
-    #print(components)
-    
+    # print(components)
+
     return _find_centroids(data, components)
-        
-        
