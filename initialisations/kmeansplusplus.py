f80d338780ab137485a50d8104ea6973edbb632e
Simon H <github@pointbeing.net>
2019-11-23 17:27:57 +0000

    Add tests and pylint compliance for kmeans++

--- initialisations/kmeansplusplus.py
+++ initialisations/kmeansplusplus.py
@@ -7,12 +7,12 @@ http://ilpubs.stanford.edu:8090/778/1/2006-13.pdf
 
 import numpy as np
 
-from initialisations.Initialisation import Initialisation
+from initialisations.base import Initialisation
 import kmeans
 
 
 class Kmeansplusplus(Initialisation):
-
+    """k-means++ initialisation algorithm"""
 
     def find_centers(self):
 
@@ -21,25 +21,21 @@ class Kmeansplusplus(Initialisation):
         centroids = np.array([self._data[randindex]])
 
         # Remaining required centroids
-        while len(centroids) < self._K:
+        while len(centroids) < self._num_clusters:
 
             distances = kmeans.distance_table(self._data, centroids)
             probabilities = distances.min(1)**2 / np.sum(distances.min(1)**2)
 
-            randindex = np.random.choice(self._num_samples, 
-                                         replace=False, 
+            randindex = np.random.choice(self._num_samples,
+                                         replace=False,
                                          p=probabilities)
             centroids = np.append(centroids, [self._data[randindex]], 0)
 
         return centroids
 
 
-## -----------------------------------------------------------------------------
-
-
-def generate(data, K, opts):
+def generate(data, num_clusters):
     """The common interface"""
-    
-    kmpp = Kmeansplusplus(data, K, opts)
+
+    kmpp = Kmeansplusplus(data, num_clusters)
     return kmpp.find_centers()
-    
---
+++ tests/initialisations/test_kmeansplusplus.py
@@ -0,0 +1,27 @@
+"""
+Tests for k-means++ initialisation algorithm
+"""
+
+import unittest
+
+import numpy as np
+
+from initialisations import kmeansplusplus as kmpp
+
+
+class KMPPTestSuite(unittest.TestCase):
+    """Test suite for random initialisation"""
+
+    def test_code_runs(self):
+        """There isn't really much more you can test here"""
+
+        data = np.array([
+            [1, 2, 3, 4],
+            [2, 3, 4, 5],
+            [3, 4, 5, 6],
+            [4, 5, 6, 7]
+            ])
+
+        centroids = kmpp.generate(data, 2)
+
+        self.assertEqual((2, 4), centroids.shape)
