9aaa49be10762f4cf9dbcdb93d91653295eadfcf
Simon H <github@pointbeing.net>
2019-11-24 12:08:24 +0000

    Minor corrections

--- tests/initialisations/test_macqueen1967.py
+++ tests/initialisations/test_macqueen1967.py
@@ -1,24 +1,23 @@
-import unittest
+"""Tests for MacQueen 1967 algorithm"""
 
-import numpy as np
+import unittest
 
 from initialisations import macqueen1967 as macqueen
 
 
-class SteinleyTestCase(unittest.TestCase):
+class MacQueenTestCase(unittest.TestCase):
 
     def test_get_pairs(self):
-    
+
         mylist = ['a', 'b', 'c']
         expected = [('a', 'b'), ('a', 'c'), ('b', 'c')]
-        
+
         pairs = macqueen.get_pairs(mylist)
         self.assertCountEqual(expected, pairs)
-        
+
         mylist = ['a', 'b', 'c', 'd']
-        expected = [('a', 'b'), ('a', 'c'), ('a', 'd'), 
+        expected = [('a', 'b'), ('a', 'c'), ('a', 'd'),
                     ('b', 'c'), ('b', 'd'), ('c', 'd')]
-        
+
         pairs = macqueen.get_pairs(mylist)
         self.assertCountEqual(expected, pairs)
-        
