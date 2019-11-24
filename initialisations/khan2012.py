188e96deb814aa3c6fd1f2a25d5489c2bdc20537
Simon H <github@pointbeing.net>
2019-11-24 12:07:38 +0000

    Fix for pylint

--- initialisations/khan2012.py
+++ initialisations/khan2012.py
@@ -1,52 +1,56 @@
+"""
+Khan 2012 initial seed selection algorithm
+
+See:
+An initial seed selection algorithm for k-means clustering of georeferenced
+data to improve replicability of cluster assignments for mapping application
+https://www.sciencedirect.com/science/article/pii/S1568494612003377
+"""
+
 import numpy as np
 from scipy.spatial import distance as spdistance
 
-'''
-Khan 2012 initial seed selection algorithm
-
-See: An initial seed selection algorithm for k-means clustering of georeferenced data to improve replicability of cluster assignments for mapping application
-https://www.sciencedirect.com/science/article/pii/S1568494612003377
-'''
 
 def sort_by_magnitude(data, column):
-    ''' Step a) Sort data by magnitude of given column'''
-    #idxs = np.argsort(np.linalg.norm(data, axis=1))
-    #return data[idxs]
+    """ Step a) Sort data by magnitude of given column"""
+    # idxs = np.argsort(np.linalg.norm(data, axis=1))
+    # return data[idxs]
 
     return np.array(sorted(data, key=lambda row: np.abs(row[column])))
 
 
 def find_distances(data, column):
-    '''Step b) Find Euclidean distances between sorted rows'''
+    """Step b) Find Euclidean distances between sorted rows"""
 
-    d = data[:,column]
+    datacol = data[:, column]
 
     distances = []
 
     for i in range(0, len(data)-1):
-        distances.append(spdistance.euclidean(d[i], d[i+1]))
+        distances.append(spdistance.euclidean(datacol[i], datacol[i+1]))
 
     return np.array(distances)
 
 
-def find_split_points(distances, K):
-    '''Step c) Find the largest distances between adjacent rows'''
+def find_split_points(distances, num_clusters):
+    """Step c) Find the largest distances between adjacent rows"""
 
-    howmany = K-1
+    howmany = num_clusters - 1
 
     return distances.argsort()[-howmany:][::-1]
-    
- 
-def generate(data, K, column):
+
+
+def generate(data, num_clusters, column):
+    """The common interface"""
 
     # Step a)
     sorteddata = sort_by_magnitude(data, column)
 
     # Step b)
-    D = find_distances(sorteddata, column)
+    distances = find_distances(sorteddata, column)
 
     # Step c)
-    splits = find_split_points(D, K)
+    splits = find_split_points(distances, num_clusters)
 
     # Step d) Find upper bounds
 
@@ -56,19 +60,15 @@ def generate(data, K, column):
     uppers = np.append(uppers, len(sorteddata))
     uppers.sort()
 
-    # Step e and f) 
+    # Step e and f)
     centroids = []
 
     lower = 0
 
-    for i in range(0, K):
-        
+    for i in range(0, num_clusters):
         upper = uppers[i]
-        
         group = sorteddata[lower:upper]
         centroids.append(np.mean(group, axis=0))
-        
         lower = upper
 
     return np.array(centroids)
-
--- tests/initialisations/test_khan2012.py
+++ tests/initialisations/test_khan2012.py
@@ -1,44 +1,54 @@
+"""
+Tests for Khan 2012 Seed Selection Algorithm
+"""
+
 import unittest
-from initialisations import khan2012 as khan
+
 import numpy as np
-import sklearn.datasets as skdatasets
+
+from initialisations import khan2012 as khan
+
 
 class KhanTestSuite(unittest.TestCase):
+    """Khan test suite"""
 
     def setUp(self):
-        self._setUpData()
+        self._set_up_data()
         self._column = 0
 
     def test_sort_by_magnitude(self):
-        np.testing.assert_equal(khan.sort_by_magnitude(self._data, self._column), self._sorted)
-
+        """Test sorting data by magnitude"""
+        np.testing.assert_equal(
+            khan.sort_by_magnitude(self._data, self._column),
+            self._sorted)
 
     def test_find_distances(self):
+        """Test finding distances between data points"""
+
         distances = khan.find_distances(self._sorted, self._column)
         expected = np.array([3, 7, 5])
         np.testing.assert_equal(distances, expected)
 
-
     def test_find_split_points(self):
+        """Test finding the split points"""
+
         distances = khan.find_distances(self._sorted, self._column)
 
         np.testing.assert_equal(khan.find_split_points(distances, 3), [1, 2])
 
+    # Utilities ---------------------------------------------------------------
 
-    # Utilities ----------------------------------------------------------------
-
-    def _setUpData(self):
+    def _set_up_data(self):
         self._data = np.array([
-                [10,10,10],
-                [ 1, 1, 1],
-                [-2,21, 2],
-                [ 5, 3, 2],
+            [10, 10, 10],
+            [1, 1, 1],
+            [-2, 21, 2],
+            [5, 3, 2],
             ])
 
         self._sorted = np.array([
-                [ 1, 1, 1],
-                [-2,21, 2],
-                [ 5, 3, 2],
-                [10,10,10],
+            [1, 1, 1],
+            [-2, 21, 2],
+            [5, 3, 2],
+            [10, 10, 10],
             ])
-
