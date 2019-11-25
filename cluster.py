579c505406c58c160c8b8180a9d05309d241e00c
Simon H <github@pointbeing.net>
2019-11-25 14:42:04 +0000

    Lint cluster + tests to 10/10

--- cluster.py
+++ cluster.py
@@ -5,41 +5,46 @@ from scipy.spatial import distance as spdistance
 
 
 class Cluster():
+    """Models a cluster and its assigned vectors"""
 
     def __init__(self):
         self._mean = None
         self._samples = []
 
-    
     def assign(self, vector, recalc_mean=True):
+        """Assign a vector to the cluster"""
+
         self._samples.append(vector)
-        
+
         if recalc_mean:
             self._calculate_mean()
-    
-    
+
     def get_samples(self):
+        """Return all vectors in the cluster"""
+
         return self._samples
-    
-    
+
     def get_mean(self):
+        """Mean/centroid of the cluster"""
+
         return self._mean
-        
-    
+
     def get_distance(self, vector):
+        """Distance between the centroid and a supplied vector"""
+
         return spdistance.euclidean(self.get_mean(), vector)
-    
-    
+
     def merge(self, other):
+        """Merge another cluster into this one"""
+
         for sample in other.get_samples():
             self.assign(sample, recalc_mean=False)
-        
+
         # just done once at the end for efficiency
         self._calculate_mean()
-        
+
     def _calculate_mean(self):
         self._mean = np.mean(np.array(self._samples), axis=0)
-        
+
     def __str__(self):
         return str(self._mean)
-
--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,89 +1,90 @@
+"""Tests for the Cluster object"""
+
 import unittest
 
 import numpy as np
 
 from cluster import Cluster
 
+# For the methods that use np.testing rather than self
+# pylint: disable=R0201
+
 
 class ClusterTestSuite(unittest.TestCase):
-
+    """Test suite for the Cluster object"""
 
     def test_assign_retrieve_1(self):
-    
-        v = [1, 2, 3]
-    
-        c = Cluster();
-        c.assign(v)
-        
-        self.assertEqual(c.get_samples(), [v])
-        
-        
+        """Test retrieving a single vector"""
+
+        vect = [1, 2, 3]
+        clus = Cluster()
+        clus.assign(vect)
+        self.assertEqual(clus.get_samples(), [vect])
+
     def test_get_mean_1(self):
-    
-        v = [1, 2, 3]
-    
-        c = Cluster();
-        c.assign(v)
-        
-        #self.assertEqual(c.get_mean(), v)
-        
-    
+        """Test calculating mean on a single vector"""
+
+        vect = [1, 2, 3]
+        clus = Cluster()
+        clus.assign(vect)
+        np.testing.assert_array_equal(clus.get_mean(), vect)
+
     def test_assign_retrieve_n(self):
-        
-        v0 = [1, 2, 3]
-        v1 = [3, 2, 1]
-        v2 = [4, 5, 6]
-    
-        c = Cluster();
-        c.assign(v0)
-        c.assign(v1)
-        c.assign(v2)    
-        
-        self.assertEqual(c.get_samples(), [v0, v1, v2])
-        
-    
+        """Test retrieving multiple vectors"""
+
+        v_0 = [1, 2, 3]
+        v_1 = [3, 2, 1]
+        v_2 = [4, 5, 6]
+
+        clus = Cluster()
+        clus.assign(v_0)
+        clus.assign(v_1)
+        clus.assign(v_2)
+
+        self.assertEqual(clus.get_samples(), [v_0, v_1, v_2])
+
     def test_get_mean_n(self):
-    
-        v0 = [2, 2, 2]
-        v1 = [3, 5, 1]
-        v2 = [4, 5, 6]
-    
-        c = Cluster();
-        c.assign(v0)
-        c.assign(v1)
-        c.assign(v2)    
-        
+        """Test calculating mean on multiple vectors"""
+
+        v_0 = [2, 2, 2]
+        v_1 = [3, 5, 1]
+        v_2 = [4, 5, 6]
+
+        clus = Cluster()
+        clus.assign(v_0)
+        clus.assign(v_1)
+        clus.assign(v_2)
+
         expected = [3, 4, 3]
-        
-        np.testing.assert_array_equal(c.get_mean(), expected)
-        
-     
+
+        np.testing.assert_array_equal(clus.get_mean(), expected)
+
     def test_get_distance_1(self):
-     
-        v = [1, 1, 1, 1]
-        
-        c = Cluster()
-        c.assign(v)
-        
-        self.assertEqual(c.get_distance([2, 2, 2, 2]), 2)
-        
-        
+        """Test Euclidean distance"""
+
+        vect = [1, 1, 1, 1]
+
+        clus = Cluster()
+        clus.assign(vect)
+
+        self.assertEqual(clus.get_distance([2, 2, 2, 2]), 2)
+
     def test_merge(self):
-    
-        a = Cluster()
-        a.assign([1, 2, 3])
-        a.assign([2, 3, 4])
-        
-        b = Cluster()
-        b.assign([3, 4, 5])
-        b.assign([4, 5, 6])
-        
-        a.merge(b)
-        
+        """Test merging clusters"""
+
+        clus_a = Cluster()
+        clus_a.assign([1, 2, 3])
+        clus_a.assign([2, 3, 4])
+
+        clus_b = Cluster()
+        clus_b.assign([3, 4, 5])
+        clus_b.assign([4, 5, 6])
+
+        clus_a.merge(clus_b)
+
         # test values (order not important)
-        self.assertCountEqual(a.get_samples(), 
-                [[1, 2, 3], [4, 5, 6], [2, 3, 4], [3, 4, 5]])
-              
+        self.assertCountEqual(clus_a.get_samples(),
+                              [[1, 2, 3], [4, 5, 6], [2, 3, 4], [3, 4, 5]])
+
         # test recalculated mean
-        np.testing.assert_array_equal(a.get_mean(), [2.5, 3.5, 4.5])
-        
+        np.testing.assert_array_equal(clus_a.get_mean(), [2.5, 3.5, 4.5])
