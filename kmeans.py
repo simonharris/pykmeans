3de649e373c191f3549302bcff1727a28d606f26
Simon H <github@pointbeing.net>
2019-12-29 13:26:25 +0000

    Lint kmeans.py + tests to 10/10

--- kmeans.py
+++ kmeans.py
@@ -1,41 +1,41 @@
-'''
+"""
 K-Means clustering algorithm
 
 See: Mirkin 2005 - Clustering for data mining: a data recovery approach
-'''
+"""
 
 import numpy as np
 
 
-def distance_table(Data, Z, columns=None):
-    '''Calculate distances between entities (1 per row) and centroids (1 per column)'''
+def distance_table(data, centroids, columns=None):
+    """Distances between entities (1 per row) and centroids (1 per column)"""
 
-    K = len(Z)
+    num_clusters = len(centroids)
 
-    AllDist = np.zeros((Data.shape[0], K))
+    distances = np.zeros((data.shape[0], num_clusters))
 
     if columns:
-        Data = Data[:, columns]
-        Z = Z[:, columns]
+        data = data[:, columns]
+        centroids = centroids[:, columns]
 
-    for k in range(K):
-        AllDist[:, k] = np.sum((Data - Z[k, :])**2, 1)
+    for k in range(num_clusters):
+        distances[:, k] = np.sum((data - centroids[k, :])**2, 1)
 
-    return AllDist
+    return distances
 
 
-def cluster(Data, K, seeds=None):
-    '''K-Means clustering algorithm rewritten'''
+def cluster(data, num_clusters, seeds=None):
+    """K-Means clustering algorithm"""
 
-    N, M = Data.shape
+    num_samples = data.shape[0]
 
     # Randomly initialise Z unless seeds are supplied
     if seeds is None:
-        Z = Data[np.random.choice(N, K, replace=False), :]
-    else:
-        Z = seeds
+        seeds = data[np.random.choice(num_samples,
+                                      num_clusters,
+                                      replace=False), :]
 
-    OldU = []  # Indices of previous nearest centroids
+    old_partition = []  # Indices of previous nearest centroids
     iterations = 0
 
     # Main loop
@@ -43,27 +43,27 @@ def cluster(Data, K, seeds=None):
 
         iterations += 1
 
-        AllDist = distance_table(Data, Z)
+        all_dist = distance_table(data, seeds)
 
-        U = AllDist.argmin(1)
+        partition = all_dist.argmin(1)
 
-        if (np.array_equal(OldU, U)):
+        if np.array_equal(old_partition, partition):
             break
 
         clusters = []
 
         # Generate new centroids and clusters
-        for k in range(K):
-            cluster = Data[U==k, :]
+        for k in range(num_clusters):
+            cluster_k = data[partition == k, :]
 
-            Z[k, :] = np.mean(cluster, 0)
-            clusters.append(cluster)
+            seeds[k, :] = np.mean(cluster_k, 0)
+            clusters.append(cluster_k)
 
-        OldU = U
+        old_partition = partition
 
-    return {'centroids':Z,
-            'labels':U,
-            'clusters':clusters,
-            'iterations':iterations,
-            'inertia':np.sum(AllDist.min(1)),
+    return {'centroids': seeds,
+            'labels': partition,
+            'clusters': clusters,
+            'iterations': iterations,
+            'inertia': np.sum(all_dist.min(1)),
             }
--- tests/test_kmeans.py
+++ tests/test_kmeans.py
@@ -1,36 +1,44 @@
+"""Unit tests for home-made k-means implementation"""
+
 import unittest
-import kmeans as mykm
+
 import numpy as np
 import sklearn.cluster as skcluster
 import sklearn.datasets as skdatasets
+
 from initialisations import erisoglu2011 as erisoglu
+import kmeans as mykm
+
 
 class DistanceTableTestSuite(unittest.TestCase):
+    """Unit tests for home-made k-means implementation"""
 
     def test_distance_table(self):
+        """Calculate matrix of distances between two sets of data points"""
 
-        data = np.array([[1,1], [2,3], [4,4]])
-        centroids = np.array([[2,2], [3,3]])
+        data = np.array([[1, 1], [2, 3], [4, 4]])
+        centroids = np.array([[2, 2], [3, 3]])
         dtable = mykm.distance_table(data, centroids)
 
-        self.assertEqual(dtable.shape, (3,2))
+        self.assertEqual(dtable.shape, (3, 2))
 
-        expected = np.array([[2,8], [1,1], [8,2]])
+        expected = np.array([[2, 8], [1, 1], [8, 2]])
         self.assertTrue(np.array_equal(dtable, expected))
 
-
     def test_vs_sklearn(self):
-        '''Compare results with scikit-learn implementation'''
+        """Compare results with scikit-learn implementation"""
 
         data = skdatasets.load_iris().data
-        K = 3
+        num_clusters = 3
 
         # Use Erisoglu as it is deterministic
-        seeds = erisoglu.generate(data, K)
+        seeds = erisoglu.generate(data, num_clusters)
 
-        mine = mykm.cluster(data, K, seeds)
+        mine = mykm.cluster(data, num_clusters, seeds)
 
-        theirs = skcluster.KMeans(n_clusters=K, n_init=1, init=seeds)
+        theirs = skcluster.KMeans(n_clusters=num_clusters,
+                                  n_init=1,
+                                  init=seeds)
         theirs.fit(data)
 
         # Assert same centroids
@@ -40,8 +48,3 @@ class DistanceTableTestSuite(unittest.TestCase):
 
         # Assert SSE calculated correctly
         self.assertEqual(mine['inertia'], theirs.inertia_)
-
-# ------------------------------------------------------------------------------
-
-if __name__ == '__main__':
-    unittest.main()
