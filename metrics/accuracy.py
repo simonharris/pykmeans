7df455c142912ce809af72a5caca10b4ccac5679
Simon H <github@pointbeing.net>
2019-12-30 11:26:30 +0000

    Lint all metrics to 10/10

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -1,8 +1,15 @@
+"""Attempt at calculating accuracy score"""
+
 import numpy as np
 import sklearn.metrics as skmetrics
 
+
 def from_matrix(mat):
+    """Calculate from a confusion matrix"""
     return np.sum(np.max(mat, 0)) / np.sum(mat)
 
+
 def score(target, found):
+    """The common interface"""
+
     return from_matrix(skmetrics.confusion_matrix(target, found))
