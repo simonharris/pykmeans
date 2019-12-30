4666fba8c9e7d63dea1762f4fa31476432587115
Simon H <github@pointbeing.net>
2019-12-30 11:22:05 +0000

    Lint all tests to 10/10

--- tests/initialisations/test_khanahmad2004.py
+++ tests/initialisations/test_khanahmad2004.py
@@ -1,3 +1,5 @@
+"""Unit tests for Khan & Ahmad 2004"""
+
 import unittest
 
 import numpy as np
@@ -5,8 +7,12 @@ import sklearn.datasets as skdatasets
 
 from initialisations import khanahmad2004 as ccia
 
+# Method could be a function
+# pylint: disable=R0201
+
 
 class KhanAhmadTestSuite(unittest.TestCase):
+    """Unit tests for Khan & Ahmad 2004"""
 
     def test_whole_thing(self):
         """Crude integration-style test until I can break it down a little"""
@@ -15,6 +21,7 @@ class KhanAhmadTestSuite(unittest.TestCase):
 
         seeds = ccia.generate(iris.data, 3)
 
+        # As emitted by the Java
         expected = np.array([[5.006, 3.428, 1.462, 0.246],
                              [6.85384615, 3.07692308, 5.71538462, 2.05384615],
                              [5.88360656, 2.74098361, 4.38852459, 1.43442623]])
--- tests/metrics/test_accuracy.py
+++ tests/metrics/test_accuracy.py
@@ -1,22 +1,38 @@
+"""Tests for my attempt at calculating Accuracy Score"""
+
 import unittest
+
 import numpy as np
+
 from metrics import accuracy
 
+
 class AccuracyScoreTest(unittest.TestCase):
+    """Tests for my attempt at calculating Accuracy Score"""
 
     def test_accuracy_score(self):
+        """Test with some trivial but real data"""
 
         # Yuan 2004 Iris
-        cm = np.array(
-                [[ 0,  0, 50],
-                 [ 3, 47,  0],
-                 [36, 14,  0]])
+        conf_matrix = np.array([
+            [0, 0, 50],
+            [3, 47, 0],
+            [36, 14, 0]
+            ])
 
-        self.assertAlmostEqual(accuracy.from_matrix(cm), 0.886667, places=6)
+        self.assertAlmostEqual(
+            accuracy.from_matrix(conf_matrix),
+            0.886667,
+            places=6)
 
         # Yuan 2004 Wine
-        cm = np.array(
-                [[31,  1, 27],
-                 [ 7, 64,  0],
-                 [11, 37,  0]])
-        self.assertAlmostEqual(accuracy.from_matrix(cm), 0.685393, places=6)
+        conf_matrix = np.array([
+            [31, 1, 27],
+            [7, 64, 0],
+            [11, 37, 0]
+            ])
+
+        self.assertAlmostEqual(
+            accuracy.from_matrix(conf_matrix),
+            0.685393,
+            places=6)
