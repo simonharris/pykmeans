b0192a718aea7f5d9e14dc9cd617b276ef36766c
Simon H <github@pointbeing.net>
2020-01-20 19:18:35 +0000

    A bit more work on K&A with notes

--- katest.py
+++ katest.py
@@ -4,9 +4,11 @@ from datasets import testloader
 from initialisations import khanahmad2004 as ka
 
 dataset = testloader.load_fossil()
+# dataset = testloader.load_iris()
 
+# Fossil and Iris both have 3
 num_clusters = 3
 
 centroids = ka.generate(dataset.data, num_clusters)
 
-print(centroids)
+#print(centroids)
