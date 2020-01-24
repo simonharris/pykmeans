28a8141cd5e1477ccf629c17b32c0515c8aaac21
Simon H <github@pointbeing.net>
2020-01-24 15:16:48 +0000

    Force no cluster smaller than 3%

--- datasets/generate.py
+++ datasets/generate.py
@@ -9,43 +9,45 @@ import os
 import numpy as np
 from sklearn.datasets import make_blobs
 
-# Ceres setup
-opts_k = [2, 5, 10, 20]
-opts_feats = [2, 10, 50, 100, 1000]
-opts_samps = [1000]
-opts_card = ['u', 'r']  # uniform or random
-opts_stdev = [0.5, 1, 1.5]
-N_EACH = 50
-OUTPUT_DIR = './synthetic/'
+ENV = 'dev'
+# ENV = 'ceres'
 
-"""
-# Dev setup
-opts_k = [20]
-opts_feats = [50]
-opts_samps = [1000]
-opts_card = ['r']  # uniform or random
-opts_stdev = [1]
-N_EACH = 5
-OUTPUT_DIR = './synthetic_simon/'
-"""
+if ENV == 'ceres':
+    # Ceres setup
+    OPTS_K = [2, 5, 10, 20]
+    OPTS_FEATS = [2, 10, 50, 100, 1000]
+    OPTS_SAMPS = [1000]
+    OPTS_CARD = ['u', 'r']  # uniform or random
+    OPTS_STDEV = [0.5, 1, 1.5]
+    N_EACH = 50
+    OUTPUT_DIR = './synthetic/'
+    N_PROCESSES = None  # defaults to os.cpu_count()
+
+else:
+    # Dev setup
+    OPTS_K = [20]
+    OPTS_FEATS = [50]
+    OPTS_SAMPS = [1000]
+    OPTS_CARD = ['r']  # uniform or random
+    OPTS_STDEV = [1]
+    N_EACH = 50
+    OUTPUT_DIR = './synthetic_simon/'
+    N_PROCESSES = 1
 
 NAME_SEP = '_'
-opts_howmany = range(0, N_EACH)
+MIN_CL_SIZE = 0.03
+WEIGHT_SHIFT = 0.005
 
 
-def gen_dataset(no_clusters, no_feats, no_samps, card, stdev, *args):
+def _gen_dataset(no_clusters, no_feats, no_samps, card, stdev):
     """Generates individual dataset"""
 
     if card == 'r':
-        weights = np.random.random(no_clusters)
-        weights /= weights.sum()
+        weights = _gen_weights(no_clusters)
         sample_cts = np.ceil(weights * no_samps).astype(int)
 
         while sample_cts.sum() > no_samps:
             sample_cts[np.argmax(sample_cts)] -= 1
-            # print(sample_cts.sum())
-
-        print(sample_cts.sum())
 
         centers = None
     else:
@@ -59,16 +61,27 @@ def gen_dataset(no_clusters, no_feats, no_samps, card, stdev, *args):
         cluster_std=stdev)
 
 
-def gen_name(config):
-    """Generates unique name for dataset"""
+def _gen_weights(num_clusters):
+    """Generate the weightings for non-uniform clustering"""
 
-    config = list(config)
-    index = config.pop()
+    weights = np.random.random(num_clusters)
+    weights /= weights.sum()
+
+    # Brute-force ensure no cluster smaller than MIN_CL_SIZE
+    while np.min(weights) < MIN_CL_SIZE:
+        weights[np.argmin(weights)] += WEIGHT_SHIFT
+        weights[np.argmax(weights)] -= WEIGHT_SHIFT
+
+    return weights
+
+
+def _gen_name(config, index):
+    """Generates unique name for dataset"""
 
     return NAME_SEP.join(map(str, config)) + NAME_SEP + f"{index:03d}"
 
 
-def save_to_disk(data, labels, name):
+def _save_to_disk(data, labels, name):
     """Save both files to disk in their own directory"""
 
     dirname = OUTPUT_DIR + name + '/'
@@ -79,27 +92,36 @@ def save_to_disk(data, labels, name):
         pass  # no problem
 
     np.savetxt(dirname + 'data.csv', data, delimiter=',')
-    np.savetxt(dirname + 'labels.csv', labels, delimiter=',')
+    np.savetxt(dirname + 'labels.csv', labels, fmt="%i", delimiter=',')
 
 
-def handler(config):
+def _handler(config):
     """The callback for the executor"""
 
-    data, labels = gen_dataset(*config)
-    save_to_disk(data, labels, gen_name(config))
+    config = list(config)
+    index = config.pop()
+
+    data, labels = _gen_dataset(*config)
+    _save_to_disk(data, labels, _gen_name(config, index))
 
     print("Done with:", config)
 
 
-# main code -------------------------------------------------------------------
+def main():
+    """Main method"""
+    configs = itertools.product(OPTS_K, OPTS_FEATS, OPTS_SAMPS,
+                                OPTS_CARD, OPTS_STDEV, range(0, N_EACH))
+
+    with futures.ProcessPoolExecutor(max_workers=N_PROCESSES) as executor:
+        res = executor.map(_handler, configs)
+
+    # Just calling this here is enough to display errors which may
+    # have been hidden behind the Executor's parallel operation
+    print(len(list(res)))
 
 
-configs = itertools.product(opts_k, opts_feats, opts_samps,
-                            opts_card, opts_stdev, opts_howmany)
+# Main ------------------------------------------------------------------------
 
-with futures.ProcessPoolExecutor() as executor:
-    res = executor.map(handler, configs)
 
-# Just calling this here is enough to display errors which may
-# have been hidden behind the Executor's parallel operation
-print(len(list(res)))
+if __name__ == '__main__':
+    main()
