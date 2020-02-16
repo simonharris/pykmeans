d48d0aa001254d2d88fae128cac784b80cf765ea
Simon H <github@pointbeing.net>
2020-02-16 20:52:41 +0000

    Add stopping condition 1; fix infinite loop where data is exactly 1 point

--- initialisations/ikmeans.py
+++ initialisations/ikmeans.py
@@ -2,14 +2,25 @@
 Intelligent K-Means clustering algorithm
 
 See: Mirkin 2005 - Clustering for data mining: a data recovery approach
+
+Notes:
+ - we skip step 1 (pre-processing) of AP as our data is standardised in
+   advance, consistently for all sets and algorithms
 """
 
 import numpy as np
 import kmeans
 
+# TODO:
+#   ??- put threshold=0 (as per Renato) and add a note to LaTex
+#   - - and do we somehow check this?
+#   DONE - change stopping condition to "run out of data"
+#   - some kind of check whether we even reach K, esp. when it's 20
+#   DONE - *then* go into selecting which ones. Renato gave two choices
+
 
 def _find_origin(data):
-    """Find the center of the data (will be 0 if standardised)"""
+    """Find the center of the data"""
 
     return np.mean(data, axis=0)
 
@@ -25,8 +36,12 @@ def _find_most_distant(data):
 def _anomalous_pattern(data):
     """Locate the most anomalous cluster in a data set"""
 
+    # Special case to discuss. Otherwise we get an infinite loop below
+    if len(data) == 1:
+        return data[0], [0]
+
     # ii) Initial setting
-    # furthest is called "c" in the paper
+    # The furthest is called "c" in the paper
     center_c, origin = _find_most_distant(data)
 
     # iii) Cluster update
@@ -36,7 +51,7 @@ def _anomalous_pattern(data):
                                          np.array([origin[0], center_c]))
         partition = all_dist.argmin(1)
 
-        # Needed later to remove from data. Callued "Ui"
+        # Needed later to remove from data. Called "Ui"
         assigned_indexes = np.where(partition == 1)[0]
 
         # Called "S" in the paper
@@ -57,22 +72,32 @@ def _anomalous_pattern(data):
 def generate(data, num_clusters):
     """The common interface"""
 
-    # i) Standardise the original data only at t=1
-    # Datasets are already standardised
-
     data_working = data
     centroids = []
+    cardinalities = []
 
-    while True:
+    # Stop condition 1
+    while len(data_working) > 0:
 
         centre, assigned_indexes = _anomalous_pattern(data_working)
-
         centroids.append(centre)
+
+        cardinalities.append(len(assigned_indexes))
 
         data_working = np.delete(data_working, assigned_indexes, 0)
 
+        # This was one of the two options for selecting which ones to use
         # Stopping condition #4
-        if len(centroids) >= num_clusters:
-            break
+        '''if len(centroids) >= num_clusters:
+            print("Got enough while data len is:", len(data_working))
+            break'''
 
-    return np.array(centroids)
+    # And this was the other option...
+    # print(cardinalities)
+
+    highest = np.argpartition(cardinalities, -num_clusters)[-num_clusters:]
+    # print(highest)
+
+    final_centroids = [centroids[i] for i in highest]
+
+    return np.array(final_centroids)
