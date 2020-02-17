93ab2ab8398294df71902d77b998ab019e6a2a1e
Simon H <github@pointbeing.net>
2020-02-17 15:27:00 +0000

    Start to add some of the  ad hoc runner scripts to git

---
+++ _run_scripts/run_intkmeans.py
@@ -0,0 +1,29 @@
+"""Temp bootstrap file to run Intelligent K-Means"""
+
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from datasets import testloader
+from initialisations import ikmeans as alg
+
+# Finds 91 centroids
+# dataset = testloader._load_local('20_1000_1000_r_1.5_025')
+# num_clusters = 20
+
+# Finds 383 centroids
+dataset = testloader._load_local('5_50_1000_r_1_025')
+num_clusters = 5
+
+# This is one that didn't complete on Ceres
+# dataset = testloader._load_local('20_2_1000_r_1_024')
+# num_clusters = 20
+
+# This one fails with < 20 centroids (19)
+# dataset = testloader._load_local('20_2_1000_r_1.5_035')
+# num_clusters = 20
+
+centroids = alg.generate(dataset.data, num_clusters)
+
+print(centroids)
+print("There were", len(centroids), "centroids found")
