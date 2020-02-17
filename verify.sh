c339b67128074d4562028ba951c65e47323fdd8d
Simon H <github@pointbeing.net>
2020-02-17 17:08:33 +0000

    Add realworld output to verify script

--- verify.sh
+++ verify.sh
@@ -6,26 +6,26 @@
 alg=$1
 dornd=$2
 
-EXP_DS=6000
 RESTARTS=50
 
+echo -e "ALGORITHM: $alg\n"
+
+## -----------------------------------------------------------------------------
+
+EXP_DS=6000
+
 if [ $dornd == "nd" ]; then
-    echo "ND"
     EXP_FILES=$((EXP_DS*RESTARTS))
 else
     EXP_FILES=$EXP_DS
 fi
 
+echo "SYNTHETIC DATASETS..."
+numlabels=`find "./_output/$alg/synthetic/" -name "labels*.csv" | wc -l`
+echo "Label files: $numlabels/$EXP_FILES"
 
-echo "ALGORITHM: $alg"
-
-numlabels=`find "./_output/$alg/" -name "labels*.csv" | wc -l`
-echo "LABELS FILES: $numlabels/$EXP_FILES"
-
-numoutputs=`find "./_output/$alg/" -name "output*.csv" | wc -l`
-echo "OUTPUT FILES: $numoutputs/$EXP_FILES"
-
-echo "Synthetic datasets..."
+numoutputs=`find "./_output/$alg/synthetic/" -name "output*.csv" | wc -l`
+echo "Output files: $numoutputs/$EXP_FILES"
 
 numdatasets=0
 for folder in `ls _output/$alg/synthetic/`; do
@@ -45,8 +45,51 @@ for folder in `ls _output/$alg/synthetic/`; do
     
     numerror=`find "./_output/$alg/synthetic/$folder" -name "exception*.csv" | wc -l`
     echo -e "\twe found $numerror exception files"
-
 done
 
-echo "TOTAL datasets: $numdatasets/$EXP_DS"
+echo -e "TOTAL datasets: $numdatasets/$EXP_DS\n"
 
+## -----------------------------------------------------------------------------
+
+
+
+EXP_DS=30
+
+if [ $dornd == "nd" ]; then
+    EXP_FILES=$((EXP_DS*RESTARTS))
+else
+    EXP_FILES=$EXP_DS
+fi
+
+MYDIR="./_output/$alg/realworld/"
+
+echo "REAL-WORLD DATASETS..."
+
+if [ -d $MYDIR ]; then
+
+    numlabels=`find $MYDIR -name "labels*.csv" | wc -l`
+    echo "Label files: $numlabels/$EXP_FILES"
+
+    numoutputs=`find $MYDIR -name "output*.csv" | wc -l`
+    echo "Output files: $numoutputs/$EXP_FILES"
+
+    counted=`ls $MYDIR | wc -l`
+
+    echo "In $MYDIR:"
+
+    echo -e "\twe found $counted dataset folders"
+
+    numlabels=`find $MYDIR -name "labels*.csv" | wc -l`
+    echo -e "\twe found $numlabels label files"
+
+    numout=`find $MYDIR -name "output*.csv" | wc -l`
+    echo -e "\twe found $numout output files"
+
+    numerror=`find $MYDIR -name "exception*.csv" | wc -l`
+    echo -e "\twe found $numerror exception files"
+
+    echo "TOTAL datasets: $counted/$EXP_DS"
+    
+ else
+    echo "No directory $MYDIR"
+ fi
