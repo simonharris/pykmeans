538e21755ed98316b8ae26ca4ab175d01999ee04
Simon H <github@pointbeing.net>
2020-02-18 23:47:25 +0000

    Add dataset and script ot reproduce Yuan vs 02 errors

---
+++ _run_scripts/run_yuan.py
@@ -0,0 +1,26 @@
+
+"""Temp bootstrap file to run K&A"""
+import pathhack
+
+from datasets import testloader
+from initialisations import yuan2004 as alg
+from preprocessors import stddise
+
+from sklearn.cluster import KMeans
+
+# dataset = testloader.load_fossil()
+# dataset = testloader.load_iris()
+
+# Didn't complete on Ceres
+dataset = testloader._load_local('2_2_1000_u_1_048')
+num_clusters = 2
+
+centroids = alg.generate(dataset.data, num_clusters)
+
+print(centroids)
+"""
+est = KMeans(n_clusters=num_clusters, init=centroids, n_init=1)
+est.fit(dataset.data)
+
+print(est.cluster_centers_)
+"""
