377d2d2283b6b3603b0176619169845621df9ea0
Simon H <github@pointbeing.net>
2020-02-19 00:07:06 +0000

    Remove superfluous standardisation; switch to problematic dataset

--- _run_scripts/run_erisoglu.py
+++ _run_scripts/run_erisoglu.py
@@ -3,18 +3,27 @@ import pathhack
 
 from datasets import testloader
 from initialisations import erisoglu2011 as alg
-from preprocessors import stddise
+# from preprocessors import stddise
+
+from sklearn.cluster import KMeans
 
 # dataset = testloader.load_fossil()
 # dataset = testloader.load_iris()
 
 # Didn't complete on Ceres
-dataset = testloader._load_local('2_1000_1000_r_0.5_009')
-
+dataset = testloader._load_local('20_2_1000_r_1.5_035')
 num_clusters = 20
 
-data = stddise.process(dataset.data)
+data = dataset.data
+
+# data = stddise.process(data)
 
 centroids = alg.generate(data, num_clusters)
 
 print(centroids)
+"""
+est = KMeans(n_clusters=num_clusters, init=centroids, n_init=1)
+est.fit(dataset.data)
+
+print(est.cluster_centers_)
+"""
