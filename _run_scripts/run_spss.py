093acb5009c6262fafaf922b8163db6356b2795e
Simon H <github@pointbeing.net>
2020-03-08 16:20:00 +0000

    SPSS implemented as per paper(s) as far as I can see

--- _run_scripts/run_spss.py
+++ _run_scripts/run_spss.py
@@ -1,16 +1,23 @@
 """Temp bootstrap file to run Pavan et al. SPSS"""
 import pathhack
 
-from datasets import testloader
-from initialisations import spss2010 as alg
+import sys
 
 from sklearn.cluster import KMeans
 
-dataset = testloader.load_iris()
-num_clusters = 3
+from datasets import testloader
+from initialisations import spss2010 as alg
 
-# dataset = testloader.load_hartigan()
-# num_clusters = 3
+dsname = ''
+if len(sys.argv) > 1:
+    dsname = sys.argv[1]
+
+if dsname == 'iris':
+    dataset = testloader.load_iris()
+    num_clusters = 3
+else:
+    dataset = testloader.load_hartigan()
+    num_clusters = 3
 
 # dataset = testloader.load_soy_small()
 # num_clusters = 4
@@ -18,10 +25,9 @@ num_clusters = 3
 data = dataset.data
 centroids = alg.generate(data, num_clusters)
 
-#print(centroids)
-"""
+print(centroids)
+
 est = KMeans(n_clusters=num_clusters, init=centroids, n_init=1)
 est.fit(dataset.data)
 
-print(est.cluster_centers_)
-"""
+print("Final centres:\n", est.cluster_centers_)
