380484d526a43babb37821660dfe783ac6df5d04
Simon H <github@pointbeing.net>
2020-03-30 00:34:44 +0100

    Finally complete the merge algorithm

--- initialisations/khanahmad2004.py
+++ initialisations/khanahmad2004.py
@@ -18,13 +18,6 @@ from sklearn.cluster import KMeans
 
 from initialisations.base import Initialisation
 
-DEBUG = True
-
-
-def debug(data):
-    if DEBUG is True:
-        print(data)
-
 
 class CCIA(Initialisation):
     """Cluster Center Initialization Algorithm"""
@@ -41,17 +34,13 @@ class CCIA(Initialisation):
             val = self._data[:, i]
 
             mystr = self._cluster_numeric_attribute(val)
-            # print("CNA:", mystr)
-
             membership = self._generate_cluster_string(mystr)
-            # print("GCS: ", membership)
 
             for sample_id in range(0, self._num_samples):
                 cluster_string[sample_id][i] = membership[sample_id]
         # end for each attribute
 
         cstr = self._extract_cluster_strings(cluster_string)
-        # print("ECS:", cstr)
 
         # Step 11
         dist_class_str = self._find_unique_cluster_strings(cstr)
@@ -71,7 +60,7 @@ class CCIA(Initialisation):
     def _cluster_numeric_attribute(self, attrib):
         """Run K-means on a single attribute"""
 
-        # "Attirbute values(s) corresponding to..."
+        # "Attribute values(s) corresponding to..."
         x_s = []
 
         # Step 2: "Compute mean and std dev..."
@@ -155,15 +144,11 @@ class CCIA(Initialisation):
         cnt = np.zeros(len(dist_class_str))
 
         for i, class_str in enumerate(cstr):
-            # print("i:", i)
+
             j = 0
 
             # for each pairs
             for key in dist_class_str:
-                # print("Key:", key)
-                # print("foo")
-                # print(str(item) + " = " + str(dist_class_str[item])
-                #      + ' --> ' + str(item == val))
 
                 if key == class_str:
                     for k in range(0, data.shape[1]):
@@ -181,100 +166,19 @@ class CCIA(Initialisation):
             return init_centers
 
         return self._merge_dbmsdc(init_centers, dist_class_str, data)
-        ## return self._my_merge_dbmsdc(init_centers) #, dist_class_str, data)
-
-    """
-    def _my_merge_dbmsdc(self, init_centers):
-
-        # Sort to match Java
-        B = init_centers[init_centers[:, 0].argsort()]
-
-        q = self._NN
-
-        print("ENTERING MERGE FOR B:\n ", B, "\n\n")
-
-        # May be needed...for now just demonstrating
-        S = []
-
-        # Step 4:
-        # "Repeat steps 5-10..."
-        # #while len(B) > self._num_clusters:
-
-        # Step 5: "For each cluster center in B..."
-        for x_i in B:
-
-            print("Looping for B with len:", len(B), "\n")
-
-            print("Start:", x_i)
-            distances = np.array([euclidean(x_i, neighbour)
-                                  for neighbour in B])
-            print("Distances:", distances)
-
-            rqxi = np.partition(distances, q)[q]
-            print("Min is:", rqxi)
-
-            rqxi_id = np.argpartition(distances, q)[q]
-            x_j = B[rqxi_id]
-            print("Point is:", x_j)
-
-            # Step 7
-            s_l = []
-
-            # Step 8, 9
-            radius = 1.5 * rqxi
-            print("Radius is:", radius)
-
-            new_distances = np.array([euclidean(x_j, neighbour)
-                                      for neighbour in B])
-            print("New distances:", new_distances)
-
-            # find IDs of points within radius
-            mypoints = np.where(new_distances < radius)[0]
-            print("Mypoints:", mypoints)
-
-            points_to_move = B[mypoints]
-            print("PTM/s_l:", points_to_move)
-
-            # s_l.append()
-            s_l = points_to_move
-
-            S.append(s_l)
-
-            B = np.delete(B, mypoints, axis=0)
-            print("Depleted B is now:", B)
-
-            print("\n\n")
-
-            if (len(B) == 0):
-                print("Len S:", len(S))
-                print("Breaking with S:", S)
-                break
-
-        return []  # no idea what to return!
-
-    """
 
     def _merge_dbmsdc(self, init_centers, dist_class_str, data):
 
-
-        # print("**Entering merge algorithm**")
-
         init_centers = init_centers[init_centers[:, 0].argsort()]
-        # print(init_centers)
 
         centers = np.zeros((self._num_clusters, self._num_attrs))
 
         B = list(range(0, len(dist_class_str)))
 
-        # print(centers)
-        # print(B)
-
-        # SFSG
-
         # Step 5: "For each cluster center..."
         for L in range(0, self._num_clusters-1):
 
-            print("\n\nLooping for L ==", L)
+            # print("\n\nLooping for L ==", L)
 
             R = np.zeros(len(B))
 
@@ -286,11 +190,9 @@ class CCIA(Initialisation):
                     distance[j] = euclidean(init_centers[i], init_centers[j])
 
                 dist_sort = sorted(distance)
-                # print("\nDISTANCES:\n", dist_sort)
                 R[i] = dist_sort[self._NN]
 
             minR = min(R)
-            print("Min R:", minR)
 
             index = 0
 
@@ -301,35 +203,28 @@ class CCIA(Initialisation):
 
             S = []
 
-            # print("Will compare to:", init_centers[index])
-
             for i in range(0, len(B)):
 
                 # B gets modified  on each loop
                 if i >= len(B):
                     break
 
-                print("WORKING B:", B)
-
                 dist = euclidean(init_centers[index], init_centers[i])
-                print("Dist:", dist)
 
                 if dist < (1.5 * minR):
                     S.append(init_centers[B[i]])
-                    print("Adding", init_centers[B[i]], "to S")
-                    # to_remove.append(i)
 
                     B = [entry for entry in B
                          if not np.array_equal(
                                  init_centers[entry],
-                                 init_centers[i])]
+                                 init_centers[B[i]])]
 
-            # print("S: ", S)
-            # print("B: ", B)
 
             centers[L] = np.mean(S, axis=0)
 
-            print(centers)
+        # Merge the remaining centers as the final center
+        temp = [init_centers[entry] for entry in B]
+        centers[L+1] = np.mean(temp, axis=0)
 
         return centers
 
