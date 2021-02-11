1f3c0ba60dace7cd5be349230dc01a47487dff4a
Simon H <github@pointbeing.net>
2021-02-11 23:58:44 +0000

    Comment fix

--- initialisations/random_c.py
+++ initialisations/random_c.py
@@ -1,8 +1,8 @@
 """
 Random Centroids initialisation
 
-Credited by Steinley 2077 to Faber 1994, but it has been around a great deal
-longer than that.
+Credited by Steinley & Brusco 2007 to Faber 1994, but it has been around a
+great deal longer than that.
 
 See: Clustering and the continuous k-means algorithm
 https://www.semanticscholar.org/paper/Clustering-and-the-Continuous-k-Means-Algorithm-Faber/94ab7d7cef96a447d15c81ac3b9f1134575785b2
