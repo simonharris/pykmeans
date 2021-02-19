9422c485d798ef1f858eaf7f178ba60f340e45a3
Simon H <github@pointbeing.net>
2021-02-19 21:32:39 +0000

    Add pc_runner

---
+++ pc_runner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python3 runner.py bradley perfcount 50 > pc_output_bradley
+python3 runner.py erisoglu perfcount 1 > pc_output_erisoglu
+python3 runner.py random_c perfcount 50 > pc_output_random_c
+python3 runner.py hand perfcount 50 > pc_output_hand
+python3 runner.py hatamlou perfcount 1 > pc_output_hatamlou
+python3 runner.py ikm_card perfcount 1 > pc_output_ikm_card
+python3 runner.py ikm_first perfcount 1 > pc_output_ikm_first
+python3 runner.py khan perfcount 50 > pc_output_khan
+python3 runner.py kkz perfcount 1 > pc_output_kkz
+python3 runner.py kmpp perfcount 50 > pc_output_kmpp
+python3 runner.py globalkm perfcount 1 > pc_output_globalkm
+python3 runner.py milligan perfcount 1 > pc_output_milligan
+python3 runner.py onoda_ica perfcount 1 > pc_output_onoda_ica
+python3 runner.py onoda_pca perfcount 1 > pc_output_onoda_pca
+python3 runner.py random_p perfcount 50 > pc_output_random_p
+python3 runner.py spss perfcount 1 > pc_output_spss
+python3 runner.py yuan perfcount 1 > pc_output_yuan
+
